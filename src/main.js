dbc78c7b0a85429d150dd06b998be268e6d08106
GustavoGA <GA17010@ues.edu.sv>
2023-06-16 11:20:15 -0600

    cambios 2

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 //url del backend recuerda quitar cualquier pleca del final / quitarla
-axios.defaults.baseURL= process.env.BACKEND || "https://backend2.gustavogutierrez.me/"
+// axios.defaults.baseURL= process.env.BACKEND || "https://backend2.gustavogutierrez.me/" regresar a esta
 //axios.defaults.baseURL= process.env.BACKEND || "http://backend.io"
-// axios.defaults.baseURL= process.env.BACKEND || "https://uesejemplo2.herokuapp.com"
+axios.defaults.baseURL= process.env.BACKEND || "https://uesejemplo2-a5f730f8fb7d.herokuapp.com"
 
 
 Vue.use(VueAxios, axios)
